76caccc280d9ffa7f7cabcf06c9a2f8c5fd0a483
Matt Cramblett <Cramblett5@gmail.com>
2020-08-06 11:53:45 -0400

    initial commit

---
+++ src/main/java/com/mattcramblett/primenumbergenerator/Main.java
@@ -0,0 +1,7 @@
+package com.mattcramblett.primenumbergenerator;
+
+public class Main {
+	public static void main(final String[] args) {
+		// Empty for now
+	}
+}
---
+++ src/test/java/com/mattcramblett/primenumbergenerator/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.mattcramblett.primenumbergenerator;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ExampleTest extends TestCase {
+
+	@Test
+	public void testCase() {
+		assertTrue(true);
+	}
+
+}
