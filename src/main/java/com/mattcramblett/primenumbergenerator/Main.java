216d2bfc80b4cf82137db331ea4458bba79dcd1e
Matt Cramblett <Cramblett5@gmail.com>
2020-08-07 10:42:58 -0400

    add Main driver program that logs result and does not validate number format

--- src/main/java/com/mattcramblett/primenumbergenerator/Main.java
+++ src/main/java/com/mattcramblett/primenumbergenerator/Main.java
@@ -1,7 +1,15 @@
 package com.mattcramblett.primenumbergenerator;
 
+import java.util.List;
+
 public class Main {
+
+	private Main() {
+	}
+
 	public static void main(final String[] args) {
-		// Empty for now
+		final List<Integer> primes = new PrimeNumberGeneratorImpl().generate(Integer.parseInt(args[0]),
+				Integer.parseInt(args[1]));
+		System.out.println(primes); // NOSONAR
 	}
 }
---
+++ src/test/java/com/mattcramblett/primenumbergenerator/MainTest.java
@@ -0,0 +1,63 @@
+package com.mattcramblett.primenumbergenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Main integration test for the application
+ */
+public class MainTest extends AbstractTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
+	private final PrintStream originalErr = System.err;
+
+	@Before
+	public void before() throws Exception {
+		// Redirect standard output so tests can verify content
+		System.setOut(new PrintStream(this.outContent));
+		System.setErr(new PrintStream(this.errContent));
+	}
+
+	@After
+	public void after() throws Exception {
+		// Restore standard output
+		System.setOut(this.originalOut);
+		System.setErr(this.originalErr);
+	}
+
+	@Test(expected = NumberFormatException.class)
+	public void testOutputWithInvalidRangeParameters() {
+		final String args[] = { "1,00", "a" };
+		Main.main(args);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testOutputWithoutRangeParameters() {
+		final String args[] = {};
+		Main.main(args);
+	}
+
+	@Test
+	public void testOutputResultWithValidRangeParameters() {
+		final String args[] = { "-100", "102" };
+		Main.main(args);
+		assertEquals(SMALL_PRIMES.toString(), this.outContent.toString().trim());
+	}
+
+	@Test
+	public void testOutputWhenResultIsEmptyWithValidRangeParameters() {
+		final String args[] = { "0", "0" };
+		Main.main(args);
+		assertEquals(Arrays.asList().toString(), this.outContent.toString().trim());
+	}
+
+}
